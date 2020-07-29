fc2d21a59f176069cc99e771a70a41c5f97894cd
Yunus Şener <yunus1183@gmail.com>
2020-07-29 21:16:40 +0300

    Added entities end repository to start

---
+++ DataAccess/Concrete/EntityFramework/Contexts/YunusSenerContext.cs
@@ -0,0 +1,21 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework.Contexts
+{
+    public class YunusSenerContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString: 
+                @"Server=(localdb)\mssqllocaldb;Database=YunusSenerDB;Trusted_Connection=True");
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<OperationClaim> OperationClaims { get; set; }
+        public DbSet<UserOperationClaim> UserOperationClaims { get; set; }
+    }
+}
---
+++ Entities/Concrete/Product.cs
@@ -0,0 +1,17 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    public class Product : IEntity
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public string QuantityPerUnit { get; set; }
+        public decimal UnitPrice { get; set; }
+        public short UnitsInStock { get; set; }
+        public bool Discontinued { get; set; }
+    }
+}
