6eeb8257d7f2548bb20857948c1a6a7588649850
Yunus Şener <yunus1183@gmail.com>
2020-08-02 23:03:29 +0300

    Added Autofac

---
+++ Business/Autofac/AutofacBusinessModule.cs
@@ -0,0 +1,15 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Autofac
+{
+    public class AutofacBusinessModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            
+        }
+    }
+}
